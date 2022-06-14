d727e86e781aeb647188275a676e8b12ba87956f
João Victor Dias Bittencourt <j.v_dias@hotmail.com>
2022-06-14 15:24:16 -0300

    get tenant reference by header or subdomain

--- src/Http/Middleware/TenantChangeConnection.php
+++ src/Http/Middleware/TenantChangeConnection.php
@@ -4,6 +4,7 @@ namespace Joaovdiasb\LaravelMultiTenancy\Http\Middleware;
 
 use Closure;
 use Joaovdiasb\LaravelMultiTenancy\Model\Tenant;
+use Str;
 
 class TenantChangeConnection
 {
@@ -20,10 +21,28 @@ class TenantChangeConnection
     public function handle($request, Closure $next)
     {
         if(!app()->runningInConsole()){
-            $reference = request()->header('X-Ref');
+            $reference = $this->getTenantReference();
             Tenant::findFirstByKey('reference', $reference)->configure()->use();
         }
 
         return $next($request);
+    }
+
+    /**
+     * Get tenant reference by header or subdomain
+     *
+     * @return string|null
+     */
+    private function getTenantReference(): ?string
+    {
+        if (request()->header('X-Ref')) {
+            return request()->header('X-Ref');
+        }
+
+        if (Str::contains(request()->getHost(), '.')) {
+            return explode('.', request()->getHost())[0];
+        }
+
+        return null;
     }
 }
